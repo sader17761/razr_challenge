394239d719c78c157539e0c4938373e73f4242fb
Corey Sader <corey.sader@gmail.com>
2017-08-23 20:24:52 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,14 +2,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser');
 
 //globals
 var port = process.env.PORT || 3010;
 
 // uses
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
 
 // spin up server
 app.listen(port, function(){
